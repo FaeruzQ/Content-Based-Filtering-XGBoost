906fea844b962f55366b071c6debbc3d99383b11
Faeruz <74523636+FaeruzQ@users.noreply.github.com>
2026-05-07 02:14:54 +0700

    Refactor app.py to improve code structure

--- book_recomendation/app.py
+++ book_recomendation/app.py
@@ -155,10 +155,10 @@ if st.button("Cari Rekomendasi"):
     st.dataframe(
         top_books[
             [
-                "relevance_probability"
+                "relevance_probability",
                 "title",
                 "author",
-                "genre",
+                "genre"
             ]
         ]
     )
