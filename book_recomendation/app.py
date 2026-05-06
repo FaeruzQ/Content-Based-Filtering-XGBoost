89af7bf82437c0690750a68f3ce144be9b66fe37
Faeruz <74523636+FaeruzQ@users.noreply.github.com>
2026-05-07 02:16:21 +0700

    Update app.py

--- book_recomendation/app.py
+++ book_recomendation/app.py
@@ -155,7 +155,7 @@ if st.button("Cari Rekomendasi"):
     st.dataframe(
         top_books[
             [
-                "relevance_probability",
+                "relevance",
                 "title",
                 "author",
                 "genre"
